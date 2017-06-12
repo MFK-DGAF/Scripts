f3fcbcc81fb7280c964d0f7807edbdadd46e93e8
MFK-DGAF <kvtobola@gmail.com>
2017-06-12 11:51:47 -0500

    1.0

---
+++ Batch/Runas/runas.bat
@@ -0,0 +1,2 @@
+runas /netonly /user:rhadata\ktobola "C:\Program Files (x86)\Microsoft SQL Server\110\Tools\Binn\ManagementStudio\Ssms.exe"
+Pause
---
+++ Batch/SQL/SQL_HADR.bat
@@ -0,0 +1,3 @@
+netsh firewall set portopening protocol = TCP port = 1434 name = SQLPort mode = ENABLE scope = SUBNET profile = CURRENT
+
+pause
