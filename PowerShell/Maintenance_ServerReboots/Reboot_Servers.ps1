68efa6e2fba5ebdfca275d11cb635a8d04467c82
MFK-DGAF <kvtobola@gmail.com>
2017-06-12 10:10:11 -0500

    1.0

---
+++ PowerShell/Maintenance_ServerReboots/Reboot_Servers.ps1
@@ -0,0 +1,20 @@
+﻿param([string]$file="E:\Automation\Maintenance_ServerReboots\Reboot_Lists\FourInTheAM_Reboots.txt")
+
+If (test-path $fFile) 
+{
+    $Reboot_List = Get-Content $file
+}
+else
+{
+    write-host ($file + "does not exist")
+}
+
+if ($Reboot_List.Count -gt 0)
+{
+    foreach ($server in $Reboot_List)
+    {
+        shutdown -r -m \\$server
+    }
+}
+
+      
