70d8378b63f49da82a81513c3c077c29b5e4565f
MFK-DGAF <kvtobola@gmail.com>
2017-06-12 10:12:07 -0500

    1.0

---
+++ PowerShell/SQLVersionCheck/SQLVersionCheck.ps1
@@ -0,0 +1,35 @@
+﻿import-module sqlps
+
+######################################################
+#Sets Variables
+######################################################
+$SRCServerList = Get-Content E:\Automation\Maintenance_HealthCheck\HealthCheckMaster\Serverlist.txt
+$DBName = "master"
+$TheDate = Get-Date
+$SQLVersionQuery=@"
+SELECT @@Version
+"@
+# array of servers with servername and the text for the email
+$MasterList = new-object system.collections.arraylist
+
+######################################################
+# Loop through the list of servers, and run the query 
+######################################################
+
+foreach ($SrcServerName in $SRCServerList)
+{
+$SQLVersion = invoke-sqlcmd -ServerInstance $SrcServerName -Database $DBName -Query $SQLVersionQuery
+$device_String = write-output ($SrcServerName + "         " +  $SQLVersion.Column1)
+$MasterList.Add($device_String)
+}
+
+######################################################
+# Emails the emails, huh, huh the emails
+#####################################################
+$smtpServer = "cheech"
+$smtpFrom = "HealthCheck@Rush-Health.com"
+$messageSubject = "SQL Version List"
+$body = "Rock the body" 
+$body = $MasterList | Out-String
+$smtpTo = "Alerts@rush-health.com"
+send-mailmessage -from "$smtpFrom" -to "$smtpTo" -subject "$messageSubject" -body "$body" -smtpServer "$smtpserver"
